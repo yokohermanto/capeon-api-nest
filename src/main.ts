9cf215479cbf940c4e2c7a3889f6a940556b3746
cacing69 <ibnuul@gmail.com>
2022-08-16 02:01:16 +0700

    revert swagger

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.13.2/swagger-ui.min.css',
-    customJs:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.13.2/swagger-ui-bundle.min.js',
-  });
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }

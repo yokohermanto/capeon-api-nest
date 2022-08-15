2a781c540c2276b58a2d75445b0277d2b5cf27a7
cacing69 <ibnuul@gmail.com>
2022-08-16 01:12:33 +0700

    add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): object {
+  getApp(): object {
     return this.appService.getResponse();
   }
 }
